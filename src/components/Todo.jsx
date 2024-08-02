f396339f53d86a06bd0c95a392c2d8e2bc377e9e
RezaMahmoudi00 <reza.mahmoudi@nexasoft.io>
2024-08-02 16:31:42 +0300

    add delete functionalitty

--- src/components/Todo.jsx
+++ src/components/Todo.jsx
@@ -2,13 +2,13 @@ import React from 'react'
 import { AiFillEdit } from 'react-icons/ai'
 import { BsFillTrashFill } from 'react-icons/bs'
 
-const Todo = ({task}) => {
+const Todo = ({task,deleteTodo}) => {
   return (
     <div className='flex justify-between items-center bg-violet-600 text-white py-3 rounded-md mb-1 cursor-pointer'>
      <p className='fontfont-primary'>{task.task} </p>
      <div className='flex items-center gap-x-4'>
       <AiFillEdit className='text-xl'/>
-      <BsFillTrashFill className='text-xl'/>
+      <BsFillTrashFill className='text-xl' onClick={()=> deleteTodo(task.id)}/>
      </div>
     </div>
   )
--- src/components/TodoList.jsx
+++ src/components/TodoList.jsx
@@ -10,12 +10,15 @@ const TodoList = () => {
   const createTodo = todo => {
     setTodo([...todoValue,{id: uuidv4(), task:todo, isEditing:false}])
   }
+  const deleteTodo = id =>{
+    setTodo(todoValue.filter(todo => todo.id !==id))
+  }
   return (
     <div className='container bg-gray-700 mt-20 p-8 rounded-md'>
       <Form createTodo ={createTodo} />
       {
         todoValue.map((todo,idx)=>
-        <Todo task={todo}key = {idx}/>
+        <Todo task={todo} key = {idx} deleteTodo ={deleteTodo}/>
         )
       }
     </div>
